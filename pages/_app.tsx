2a62be9eb310635c332d639721fcd7fed674d4ca
Peter Li <li.peter7122@gmail.com>
2023-10-07 13:28:09 -0400

    skeleton framework

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,27 @@
 import '@/styles/globals.css'
+import '@/styles/theme.css'
 import type { AppProps } from 'next/app'
+import type { ReactElement, ReactNode } from 'react';
+import type { NextPage } from 'next';
+import { useState, createContext, useEffect } from 'react';
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+interface ConnexConextInterface {
+  thor: any;
+  vendor: any;
+};
+
+export const ConnexContext = createContext<ConnexConextInterface>({
+  thor: null,
+  vendor: null,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
---
+++ pages/scan/scan.tsx
@@ -0,0 +1,4 @@
+export default function Scan() {
+    return (<>
+    </>)
+}
